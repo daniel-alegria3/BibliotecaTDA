89e8d03b1ad121a62d3573e7e1095f9140cea166
Daniel Alegria Sallo <ccusdanielalegriasallo@gmail.com>
2023-07-17 10:21:18 -0500

    added stack and queue classes

---
+++ Cola.cs
@@ -0,0 +1,58 @@
+﻿namespace BibliotecaTDA;
+
+public class Cola
+{
+    private object? valor;
+    private Cola? subcabeza;
+
+    public Cola ( )
+    {
+        valor = null;
+        subcabeza = null;
+    }
+
+    public Cola ( object? valor, Cola? subcabeza )
+    {
+        this.valor = valor;
+        this.subcabeza = subcabeza;
+    }
+
+    public bool es_vacio ( )
+    {
+        return valor == null && subcabeza == null;
+    }
+
+    public bool es_lleno ( )
+    {
+        return false;
+    }
+
+    public void colar ( object? val )
+    {
+        if ( es_vacio() ) {
+            valor = val;
+            subcabeza = new Cola();
+            return;
+        }
+        subcabeza.colar( val );
+    }
+
+    public object? desacolar ( )
+    {
+        if ( es_vacio() )
+            return null;
+
+        object? val = valor;
+        valor = subcabeza.valor;
+        subcabeza = subcabeza.subcabeza;
+
+        return val;
+    }
+
+    public object? cabeza ( )
+    {
+        return valor;
+    }
+
+}
+
---
+++ Pila.cs
@@ -0,0 +1,49 @@
+﻿namespace BibliotecaTDA;
+
+public class Pila
+{
+    private object? valor;
+    private Pila? subpila;
+
+    public Pila ()
+    {
+        valor = null;
+        subpila = null;
+    }
+
+    public Pila ( object? valor, Pila? subpila )
+    {
+        this.valor = valor;
+        this.subpila = subpila;
+    }
+
+    public bool es_vacio ( )
+    {
+        return valor == null && subpila == null;
+    }
+
+    public void apilar ( object? val )
+    {
+        subpila = new Pila ( valor, subpila );
+        valor = val;
+    }
+
+    public object? desapilar ( )
+    {
+        if ( es_vacio() )
+            return null;
+
+        object? val = valor;
+        valor = subpila.valor;
+        subpila = subpila.subpila;
+
+        return val;
+    }
+
+    public object? cima ( )
+    {
+        return valor;
+    }
+
+}
+
